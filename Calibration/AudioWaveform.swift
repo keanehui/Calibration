ca2944d66fe4df67a65a953b028454a3e117b4f3
keanehui <kawonghui@gmail.com>
2022-04-01 13:19:40 +0800

    waveform improvement

--- Calibration/AudioWaveform.swift
+++ Calibration/AudioWaveform.swift
@@ -14,21 +14,24 @@ struct AudioWaveform: View {
         VStack {
             ZStack(alignment: .center) {
                 Group {
-//                    WaveForm(color: .red, amplify: 140, isReverse: true)
+//                    WaveForm(color: .red, amplify: 150)
+//                        .reverse()
 //                        .offset(x: UIScreen.main.bounds.width*0.5, y: 0)
-//                    WaveForm(color: .red, amplify: 140, isReverse: true)
+//                    WaveForm(color: .red, amplify: 150)
+//                        .reverse()
 //                        .offset(x: -UIScreen.main.bounds.width*0.5, y: 0)
-                    WaveForm(color: .purple, amplify: 150, isReverse: false)
-                    WaveForm(color: .cyan, amplify: 140, isReverse: true)
-                    WaveForm(color: .green, amplify: 150, isReverse: false)
+                    WaveForm(color: .purple, amplify: 150)
+                        .reverse()
+                    WaveForm(color: .green, amplify: 150)
                         .offset(x: UIScreen.main.bounds.width*0.5, y: 0)
-                    WaveForm(color: .green, amplify: 150, isReverse: false)
+                    WaveForm(color: .green, amplify: 150)
                         .offset(x: -UIScreen.main.bounds.width*0.5, y: 0)
+                    WaveForm(color: .cyan, amplify: 140)
                 }
-                .opacity(0.5)
-                Image(systemName: "mic.circle.fill")
+                .opacity(0.7)
+                Image(systemName: "waveform.and.mic")
                     .foregroundColor(.white)
-                    .font(.system(size: 60, weight: .light, design: .rounded))
+                    .font(.system(size: 50, weight: .light, design: .rounded))
                     .offset(x: 0, y: 10)
             }
             .frame(maxWidth: .infinity, maxHeight: 100)
@@ -44,7 +47,6 @@ struct AudioWaveform: View {
 struct WaveForm: View {
     var color: Color
     var amplify: CGFloat
-    var isReverse: Bool = false
     
     var body: some View {
         TimelineView(.animation) { timeLine in
@@ -53,7 +55,7 @@ struct WaveForm: View {
                 let angle = timeNow.remainder(dividingBy: 2)
                 let offset = angle * size.width
                 
-                context.translateBy(x: isReverse ? -offset : offset, y: 0)
+                context.translateBy(x: offset, y: 0)
                 context.fill(getPath(size: size), with: .color(color))
                 context.translateBy(x: -size.width, y: 0)
                 context.fill(getPath(size: size), with: .color(color))
@@ -71,10 +73,14 @@ struct WaveForm: View {
             let midHeight = size.height / 2
             let width = size.width
             path.move(to: CGPoint(x: 0, y: midHeight))
-            path.addCurve(to: CGPoint(x: width, y: midHeight), control1: CGPoint(x: width*0.4, y: midHeight+amplify), control2: CGPoint(x: width*0.65, y: midHeight-amplify))
+            path.addCurve(to: CGPoint(x: width, y: midHeight), control1: CGPoint(x: width*0.5, y: midHeight+amplify), control2: CGPoint(x: width*0.5, y: midHeight-amplify))
             path.addLine(to: CGPoint(x: width, y: size.height))
             path.addLine(to: CGPoint(x: 0, y: size.height))
         }
+    }
+    
+    func reverse() -> some View {
+        return self.rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
     }
 }
 
